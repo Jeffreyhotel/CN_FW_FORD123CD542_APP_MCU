30bb2da9eecd3895cc1fc2053380c80ebecacce2
AUO ChengyuChen <jeffreychen@alum.ccu.edu.tw>
2024-10-23 10:12:37 +0800

    [Modify] Add OTA Fool-proof Flash Flag [FF,FF,12,30] Add.

--- app/inc/FlashApp.h
+++ app/inc/FlashApp.h
@@ -9,6 +9,16 @@
 #define ADDR_MCUFLASH_SNUMBER   0x0001F100U
 #define ADDR_MCUFLASH_DIMMING   0x0001F200U
 
+#if (MCU_POSITION == 0x02U)
+	#define PRJ_INFO_SECTION ".prj.info.2"
+#elif (MCU_POSITION == 0x0AU)
+	#define PRJ_INFO_SECTION ".prj.info.A"
+#elif (MCU_POSITION == 0x0BU)
+	#define PRJ_INFO_SECTION ".prj.info.B"
+#else
+	#error MCU_POSITION IS NOT DEFINED OR INVALID
+#endif
+
 void FlashApp_WriteRowFlash(uint8_t data[], const uint32_t address, uint8_t length);
 
 #endif
